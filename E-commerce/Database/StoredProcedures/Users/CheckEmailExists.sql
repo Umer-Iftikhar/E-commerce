18d3eb2a1b5cfdc21d471ed1b224a0a70aa6982b
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-07-13 14:14:41 +0500

    Add CheckEmailExists stored procedure

---
+++ E-commerce/Database/StoredProcedures/Users/CheckEmailExists.sql
@@ -0,0 +1,31 @@
+CREATE PROCEDURE dbo.CheckEmailExists
+    @Email VARCHAR(250)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+        IF EXISTS (SELECT 1 FROM dbo.Users WHERE Email = @Email)
+        BEGIN
+            SELECT 
+                409 AS ResponseCode,
+                'Email Already Exists' AS ResponseMessage;
+        END
+        ELSE
+        BEGIN
+            SELECT 
+                200 AS ResponseCode,
+                'Email Is Available' AS ResponseMessage;
+        END
+
+    END TRY
+    BEGIN CATCH
+
+        SELECT 
+            500 AS ResponseCode,
+            ERROR_MESSAGE() AS ResponseMessage;
+
+    END CATCH
+END
+GO
